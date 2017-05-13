d64a8d38c79e9f5ad54fd3e381d67b5042e00125
daniel <daniel@nowbusking.com>
2017-05-13 13:56:48 +0900

    5_13 Url 및 View

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.user')
     title = models.CharField(max_length=200)
-    image = models.CharField(max_length=200)
+  #  image = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
     published_at = models.DateTimeField(blank=True, null=True)

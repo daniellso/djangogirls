26fc8d08e1475feb9050999caedded5478f68e11
daniel <daniel@nowbusking.com>
2017-05-20 18:33:18 +0900

    view 재 설정

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -9,5 +8,11 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('published_at')
+    posts = Post.objects.filter(
+        published_at__lte=timezone.now()).order_by('published_at')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
